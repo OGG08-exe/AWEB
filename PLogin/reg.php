f8143c1f052ec202aa8e86474fd6343b1bae82e3
OGG08-exe <olivergomezgarcia2008@gmail.com>
2025-11-07 17:04:58 +0100

    reg.php añadido

---
+++ PLogin/reg.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UserLogin</title>
+    <link rel="stylesheet" href="reg.css">
+</head>
+<body>
+    <h1>Facebook</h1>
+    <h1>Registrarse</h1> 
+    <h2>¿Ya tienes cuenta?</h2>
+        <a href="Login.php">Inicia sesión aqui</a>
+    <div>
+        <br>
+<form action="Proreg.php" method="post">
+<label>Usuario:</label>
+<input type="text" name="usuario" required><br><br>
+<label>Contraseña:</label>
+<input type="password" name="password" required><br><br>
+     <br>
+        <button type="submit">Completar Registro</button>
+</form>
+
+    </div>
+    </body>
+</html>
