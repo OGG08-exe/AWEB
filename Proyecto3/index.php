9222bf3fe144761c50f63ced9195cf3e165d2dac
OGG08-exe <olivergomezgarcia2008@gmail.com>
2025-12-10 20:24:06 +0100

    comit1

---
+++ Proyecto3/index.php
@@ -0,0 +1,16 @@
+<?php include "db.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gestor de usuarios</title>
+
+</head>
+<body>
+<div class="container">
+    <h1>Gestor de usuarios</h1>
+    <a class="btn" href="list.php">ir al CRUD</a>
+</div>
+</body>
+</html>
---
+++ proyecto_2/conexion.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "OGGO";
+$pass = "P@ssw0rd";
+$db = "Proyecto_login";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
+}
+?>
